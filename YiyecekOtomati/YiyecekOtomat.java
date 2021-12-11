592467525ad3ba368863d5f9637f7567f33b316c
mehlika <94947014+mehlikaa@users.noreply.github.com>
2021-12-10 23:30:52 -0800

    Yiyecek_Otomati
    Kenan Beyden Pazar Grubu

---
+++ YiyecekOtomati/YiyecekOtomat.java
@@ -0,0 +1,13 @@
+package YiyecekOtomati;
+
+public class YiyecekOtomat {
+
+	static List <String> ArrayList<>();
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
